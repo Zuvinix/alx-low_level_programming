76bc00b096610b70814adaa6241ed4f85dcecf94
Samuel <samueldivine2000@gmail.com>
2022-10-16 18:42:46 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Describes start of program
+ *
+ * Return: Prints zero if there is no error otherwise print non-zero value
+ */
+
+int main(void)
+
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+		return (0);
+}
