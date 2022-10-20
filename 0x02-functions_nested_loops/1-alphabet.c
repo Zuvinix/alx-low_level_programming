0f1b8f2c0d8e9f9c204b70460651fe8d54158f77
Samuel <samueldivine2000@gmail.com>
2022-10-19 21:10:59 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * print alphabets from a to z
- *
+ * print_alphabet - print all alphabets in lowercase
  */
 
 void print_alphabet(void)
 {
 	char letter;
+
+
 	for (letter = 'a'; letter <= 'z'; letter++);
 	_putchar(letter);
+
 	_putchar('\n');
 }
