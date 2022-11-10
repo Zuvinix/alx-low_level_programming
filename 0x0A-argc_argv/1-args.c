7b2579e036b026c46be250f713c4c2f16e5cecd8
Samuel <samueldivine2000@gmail.com>
2022-11-10 15:21:50 -0800

    task 0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,9 @@
  * Return: Always 0 if success
  */
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
-printf("%d\n" , argc - 1);
+printf("%d\n", argc - 1);
 return (0);
 }
